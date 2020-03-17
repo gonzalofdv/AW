8a5373cc7fa0e97c2eb8c33067b4d200fde02dc1
gonzalofdv <gfigue01@ucm.es>
2020-03-17 19:10:22 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilo.css" />
+	<meta charset="utf-8">
+	<meta http-equiv="Expires" content="0">
+	<meta http-equiv="Last-Modified" content="0">
+	<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+	<meta http-equiv="Pragma" content="no-cache">
+	<title>Portada</title>
+</head>
+
+<body>
+
+	<?php
+		require("cabecera.php");
+	?>
+
+	<div id="contenido">
+		<h1>Página principal</h1>
+		<p> Aquí está el contenido público, visible para todos los usuarios. </p>
+	</div>
+
+	<?php
+		require("sidebarDer.php");
+		require("pie.php");
+	?>
+<!-- Fin del contenedor -->
+
+</body>
+</html>
