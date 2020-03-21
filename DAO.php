840aec79fc3ae645026f44271004c9f7e8babdf0
fernag08 <62303968+fernag08@users.noreply.github.com>
2020-03-21 19:48:24 +0100

    Create DAO.php

---
+++ DAO.php
@@ -0,0 +1,27 @@
+<?php
+
+class DAO {
+    
+	protected $db; // Tenemos un atributo público que será la BBDD
+	
+ 	public function __construct(){
+        if (!$this->db)
+        {
+            $this->db = new mysqli('localhost', 'beer', 'admin', 'beer');
+            if ($this->db->connect_errno) {
+                echo "Fallo al conectar a MySQL: (" . $this->db->connect_errno . ") " . $this->db->connect_error ;
+            }
+            
+            if(!$this->db->set_charset("utf8")) {
+                printf("<hr>Error loading character set utf8 (Err. nº %d): %s\n<hr/>",  $this->mysqli->errno, $this->mysqli->error);
+                exit();
+            }
+            
+            ini_set('default_charset', 'UTF-8');
+		}
+        
+        if (!$this->db) {
+            echo "fail";
+        }
+    }
+?>
