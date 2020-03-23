b1de62cf4fde75dbb6b31bbba19fc1a505e53529
gonzalofdv <gfigue01@ucm.es>
2020-03-23 14:31:46 +0100

    ficheros creados

---
+++ PreguntaSA.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('PreguntaDAO.php');
+require_once('PreguntaTransfer.php');
+
+class PreguntaSA {
+
+	
+
+}
+
+?>
---
+++ PreguntaTransfer.php
@@ -0,0 +1,33 @@
+<?php
+
+class PreguntaTransfer {
+	private $pregunta="";
+	private $codLiga="";
+
+	public function __construct(){}
+
+	public function __construct($pregunta, $codLiga){
+		$this->pregunta = $pregunta;
+		$this->codLiga = $codLiga;
+	}
+
+	//HE PUESTO 2 CONSTRUCTORES 1 VACIO POR QUE DE MOMENTO NO SE AÑADIRAN PREGUNTAS Y OTRO LLENO PARA QUE EN EL FUTURO PODAMOS INCLUIR LA FUNCION DE QUE EL USUARIO SOMOSFAMILIA PUEDA INCLUIR PREGUNTAS (SI OS PARECE BIEN)
+
+	public function getPregunta(){
+		return $this->pregunta;
+	}
+
+	public function setPregunta($pregunta){
+		$this->pregunta = $pregunta;
+	}
+
+	public function getcodLiga(){
+		return $this->codLiga;
+	}
+
+	public function setcodLiga($codLiga){
+		$this->codLiga = $codLiga
+	}
+}
+
+?>
