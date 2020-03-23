844d2b684a0e86c4f82e9908482842b2938aa230
juancar99 <jurosado@ucm.es>
2020-03-23 19:15:42 +0100

    Update logout.php

---
+++ logout.php
@@ -0,0 +1,41 @@
+<?php 
+	session_start();
+	unset($_SESSION); 
+	session_destroy(); 
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilo.css" />
+	<meta charset="utf-8">
+	<meta http-equiv="Expires" content="0">
+	<meta http-equiv="Last-Modified" content="0">
+	<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+	<meta http-equiv="Pragma" content="no-cache">
+	<title>Logout</title>
+</head>
+
+<body>
+
+<div id="contenedor">
+
+	<?php 
+		require("cabecera.php"); 
+	?>
+
+	<div id="contenido">
+
+		<h1>Has cerrado sesion</h1>
+
+	</div>
+
+	<?php 
+		include("sidebarDer.php"); 
+		include("pie.php"); 
+	?>
+
+</div> 
+
+</body>
+</html>
