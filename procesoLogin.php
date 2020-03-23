ea040b420299cf382e3eb4177bad0da7dd0f73ea
juancar99 <jurosado@ucm.es>
2020-03-23 14:18:11 +0100

    Update procesoLogin.php

--- procesoLogin.php
+++ procesoLogin.php
@@ -11,25 +11,15 @@ $pass=$_POST['password'];
 if((!empty($usu)) && (!empty($pass))){
 	$p = new UsuarioTransfer("", "", "", "", "", $usu,$pass,"", 0, 0); //Constructora UsuarioTransfer, pasándole solo estosatributos se inicia sesión
 	$usuarioSA = new UsuarioSA();
-<<<<<<< HEAD
 	$check= $usuarioSA ->checkUsuario($p);
 		if(!$check){
 			$_SESSION['login']=True;
 			$_SESSION['nombre'] = $usu;
+			header("refresh:3; url=index.php");
 		}
 		else{
 			echo"El usuario introducio no existe" . "<br>" . "Redireccionando en 3 segundos..";
 			header("refresh:3; url=registro.php");
-=======
-	$check=$usuarioSA ->checkUsuario($p);
-		if(!$check){
-			$_SESSION['login'] = true;
-			$_SESSION['nombre'] = $usu;
-		}
-		else{
-				echo"El usuario introducido no existe" . "<br>" . "Redireccionando en 3 segundos..";
-				header("refresh:3; url=registro.php");
->>>>>>> e367e1fa074fa49e6a1f3d04a6fd6d7de6c18542
 		}
 }
 else{
