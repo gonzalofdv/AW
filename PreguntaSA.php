8b8a92f8c7e3be63b630cc94eb2ca2ff62c7d72f
gonzalofdv <gfigue01@ucm.es>
2020-03-24 13:23:26 +0100

    a

--- PreguntaSA.php
+++ PreguntaSA.php
@@ -5,8 +5,15 @@ require_once('PreguntaTransfer.php');
 
 class PreguntaSA {
 
-	
+	protected $preguntaDAO;
 
+	public function newPregunta(PreguntaTransfer $preg){
+		if(!$this->preguntaDAO){
+			$this->preguntaDAO = new PreguntaDAO();
+		}
+		$aux = $this->preguntaDAO;
+		return $aux->insert($preg);
+	}
 }
 
 ?>
---
+++ formularioPreguntaRespuesta.php
@@ -0,0 +1,51 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilo.css" />
+	<meta charset="utf-8">
+	<meta http-equiv="Expires" content="0">
+	<meta http-equiv="Last-Modified" content="0">
+	<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+	<meta http-equiv="Pragma" content="no-cache">
+	<title>Registro</title>
+</head>
+<body>
+	<?php
+		require("cabecera.php");
+	?>
+	<div id="contenido">
+	<br>
+	<form action="procesarNuevaPregRes.php" method="post">
+	<fieldset>
+		<legend>Nueva Pregunta</legend>
+			Pregunta:<br> <input type="text" name="preg"><br>
+			Selecciona la liga a la que pertenece:<br>
+			<select name="liga">
+				<option value="0">Ligas:</option>
+				<?php
+					$db=@mysqli_connect('localhost', 'userLocal', 'Ua8smYv6GzqsNnsy', 'varderindecorner');
+					$q = mysqli_query($db, "SELECT * FROM ligas");
+					while($valores = mysqli_fetch_array($q)){
+						echo '<option value="' . $valores[0] . '"> ' . $valores[1] . '</option>';
+					}
+				?>
+			</select>
+			<br>
+
+			Respuesta correcta:<br> <input type="text" name="v"><br>
+			Respuesta falsa 1:<br> <input type="text" name="f1"><br>
+			Respuesta falsa 2: <br> <input type="text" name="equipo"><br>
+			
+			<input type="checkbox" name="condi" value="ok">Confirmar enviar pregunta.<br>
+			<input type="submit" name="aceptar">	
+	</fieldset>
+	</form>
+	</div>
+	<?php
+		require("sidebarDer.php");
+		require("pie.php");
+	?>
+</body>
+</html>
