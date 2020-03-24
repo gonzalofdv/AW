7c129547d829f5cf301bb6007398e0e19a8e0170
fernag08 <62303968+fernag08@users.noreply.github.com>
2020-03-24 19:45:47 +0100

    Update formularioNoticia.php

--- formularioNoticia.php
+++ formularioNoticia.php
@@ -1,3 +1,15 @@
+<?php session_start(); ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilo.css" />
+	<meta charset="utf-8">
+	<meta http-equiv="Expires" content="0">
+	<meta http-equiv="Last-Modified" content="0">
+	<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+	<meta http-equiv="Pragma" content="no-cache">
+	<title>Registro</title>
 </head>
 <body>
 	<?php
