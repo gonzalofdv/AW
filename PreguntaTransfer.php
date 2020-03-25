c04c311380bf5898f95db3fd126b85be0278bff2
fernag08 <62303968+fernag08@users.noreply.github.com>
2020-03-25 19:57:45 +0100

    Update PreguntaTransfer.php

--- PreguntaTransfer.php
+++ PreguntaTransfer.php
@@ -1,19 +1,14 @@
 <?php
 
 class PreguntaTransfer {
-	private $idPregunta;
 	private $pregunta="";
 	private $codLiga="";
-
-	public function __construct(){}
 
 	public function __construct($pregunta, $codLiga){
 		$this->pregunta = $pregunta;
 		$this->codLiga = $codLiga;
 	}
-
-	//HE PUESTO 2 CONSTRUCTORES 1 VACIO POR QUE DE MOMENTO NO SE AÑADIRAN PREGUNTAS Y OTRO LLENO PARA QUE EN EL FUTURO PODAMOS INCLUIR LA FUNCION DE QUE EL USUARIO SOMOSFAMILIA PUEDA INCLUIR PREGUNTAS (SI OS PARECE BIEN)
-
+	
 	public function getPregunta(){
 		return $this->pregunta;
 	}
@@ -27,7 +22,7 @@ class PreguntaTransfer {
 	}
 
 	public function setCodLiga($codLiga){
-		$this->codLiga = $codLiga
+		$this->codLiga = $codLiga;
 	}
 }
 
