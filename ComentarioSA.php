c726fa3b2f2a8683b0da47f73425c0235a5f0645
fernag08 <62303968+fernag08@users.noreply.github.com>
2020-03-26 20:55:08 +0100

    Create ComentarioSA.php

---
+++ ComentarioSA.php
@@ -0,0 +1,46 @@
+<?php
+require_once('ComentarioDAO.php');
+require_once('ComentarioTransfer.php');
+
+class ComentarioSA {
+
+	// Atributos
+    protected $comentarioDAO;
+	
+	public function insertComentario(ComentarioTransfer $comentario){
+		if(!$this->comentarioDAO){
+			$this->comentarioDAO = new ComentarioDAO();
+		}
+		$aux = $this->comentarioDAO;
+		return $aux->insert($comentario);
+	}
+	
+	public function updateComentario(ComentarioTransfer $comentario){
+		if(!$this->comentarioDAO){
+			$this->comentarioDAO = new ComentarioDAO();
+		}
+		$aux = $this->comentarioDAO;
+		$aux->update($comentario);
+	}
+	
+	public function deleteComentario(ComentarioTransfer $comentario){
+		if(!$this->comentarioDAO){
+			$this->comentarioDAO = new ComentarioDAO();
+		}
+		$aux = $this->comentarioDAO;
+		$aux->delete($comentario);
+	}
+
+	public function devuelveComentarios(){
+		if(!$this->comentarioDAO){
+			$this->comentarioDAO = new ComentarioDAO();
+		}
+		$aux = $this->comentarioDAO;
+		$res = $aux->devuelveComentarios();
+
+		return $res;
+	}
+	
+}
+
+?>
