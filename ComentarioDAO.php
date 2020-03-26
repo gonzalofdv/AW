9ff84d5930955de2700f1999d3065ff79222e84c
fernag08 <62303968+fernag08@users.noreply.github.com>
2020-03-26 20:55:06 +0100

    Create ComentarioDAO.php

---
+++ ComentarioDAO.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once('ComentarioTransfer.php');
+require_once('DAO.php');
+
+class ComentarioDAO extends DAO{
+
+	//Metodos
+	
+	public function insert(ComentarioTransfer $c){
+		$db = $this->db;
+		
+		$codUsuario = $c->getCodUsuario();
+		$codNoticia = $c->getCodNoticia();
+		$comentario = $c->getComentario();
+		
+		$sql = "INSERT INTO comentarios (CodNoticia, CodUsuario, Comentario) VALUES ('$codNoticia', '$codUsuario', '$comentario')";
+		$consulta = mysqli_query($db, $sql);
+		
+		if($consulta){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+	public function devuelveComentarios(){
+		$db = $this->db;
+		$sql = "SELECT * FROM comentarios";
+		$res = mysqli_query($db, $sql);
+		return $res;
+	}
+	
+}
+	
+?>
