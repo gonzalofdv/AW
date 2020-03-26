613fb7c7493dd22746d9936da20cbe44db1fe6ad
juancar99 <jurosado@ucm.es>
2020-03-26 20:45:18 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,25 +2,8 @@
 	session_start();
 	unset($_SESSION); 
 	session_destroy(); 
-?>
 
-<!DOCTYPE html>
-<html>
-<head>
-	<link rel="stylesheet" type="text/css" href="estilo.css" />
-	<meta charset="utf-8">
-	<meta http-equiv="Expires" content="0">
-	<meta http-equiv="Last-Modified" content="0">
-	<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
-	<meta http-equiv="Pragma" content="no-cache">
-	<title>Logout</title>
-</head>
-
-<body>
-		<p>Gracias, ¡Vuelva pronto!</p><br> 
-		<p>Redireccionando en 3 segundos..</p>
-		<?php
+		echo"Gracias, ¡Vuelva pronto!" . "<br>" . "Redireccionando en 3 segundos..";
 		header("refresh:3; url=index.php");
-		?>
-</body>
-</html>
+		
+?>
