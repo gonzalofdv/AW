3911c550391ec5a1468a362a5bcf0b3a472d2513
JorgeBorja <jorborja@ucm.es>
2020-03-28 21:05:03 +0100

    muchisimos mas cambios

--- sidebarDer.php
+++ sidebarDer.php
@@ -1,7 +1,18 @@
 	<div id="sidebar-right">
-		<?php
-			if (isset($_SESSION["login"]) && ($_SESSION["login"]==true)) {
-				echo "<h2><a href='mostrarPerfil.php'>Mi perfil</a></h2>";
-			}
-		?>
+	
+		<div id="perfil">
+			<?php
+				if (isset($_SESSION["login"]) && ($_SESSION["login"]==true)) {
+					echo "<h2><a href='mostrarPerfil.php'>Mi perfil</a></h2>";
+				}
+			?>
+		</div>
+		
+		<div id="anuncios">
+			<img src="./img/insparya.jpg" alt="clinica capilar cr7" width="350">
+			<img src="./img/anuncio2.jpg" alt="anuncio alcohol" width="350">
+			<!--<img src="./img/oreo.jpg" alt="galletas oreo" width="350">-->
+			
+		</div>
+		
 	</div>
