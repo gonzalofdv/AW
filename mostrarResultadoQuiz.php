d525f9cc1aadad8654dcb0422a974880345e7ec2
juancar99 <jurosado@ucm.es>
2020-03-30 10:38:18 +0200

    Create mostrarResultadoQuiz.php

---
+++ mostrarResultadoQuiz.php
@@ -0,0 +1,46 @@
+<?php
+session_start(); 
+
+$aciertos = $_GET['aciertos'];?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="estilo.css" />
+	<meta charset="utf-8">
+	<meta http-equiv="Expires" content="0">
+	<meta http-equiv="Last-Modified" content="0">
+	<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+	<meta http-equiv="Pragma" content="no-cache">
+	<title>Quiz</title>
+</head>
+
+<body>
+
+	<?php
+		require("cabecera.php");
+	?>
+
+	<div id="contenido">		
+	<?php
+	if($aciertos==10){
+		echo "<h2>¡Enhorabuena!, has conseguido acertar todas las preguntas del Quiz. Sumarás 20 puntos a tu registro</h2>";
+	}
+	else if($aciertos > 0 $$ $aciertos < 10){
+		echo "<h2>¡Enhorabuena!, has conseguido acertar".$aciertos."pregunta/s del Quiz. Sumarás".$aciertos."punto/s a tu registro</h2>";
+	}
+	else{
+		echo "<h2>Lo sentimos, no has conseguido responder correctamente a ninguna pregunta.¡Sigue intentándolo!</h2>";
+	}
+	?>
+
+	</div>
+
+	<?php
+		require("sidebarDer.php");
+		require("pie.php");
+	?>
+
+
+</body>
+</html>
