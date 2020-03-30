27cab387575a70f5f95dc907c1c44b610222ba23
gonzalofdv <gfigue01@ucm.es>
2020-03-30 18:17:06 +0200

    Update sidebarDer.php

--- sidebarDer.php
+++ sidebarDer.php
@@ -7,9 +7,47 @@
 				}
 			?>
 		</div>
+
+		<div id="ranking">
+
+			<?php
+
+				require_once('UsuarioSA.php');
+
+				$usuarioSA = new UsuarioSA();
+				$lista = $usuarioSA->devuelveRanking();
+
+			?>
+
+			<table border="2">
+				<caption><b>TOP 10 USUARIOS</b></caption>
+				<tr>
+					<td><b>Puesto</b></td>	
+					<td><b>Usuario</b></td>
+					<td><b>Puntos</b></td>
+				</tr>
+
+			<?php
+				$i = 1;
+				while($mostrar=mysqli_fetch_object($lista)){
+					echo "<tr>";
+						echo "<td>".$i."º</td>";
+						echo "<td>".$mostrar->NombreUsuario."</td>";
+						echo "<td>".$mostrar->Puntos."</td>";
+					echo "</tr>";
+
+					$i++;
+				}
+
+			?>		
+			</table>
+			<br>
+
+		</div>
 		
 		<div id="anuncios">
-			<img src="./img/insparya.jpg" alt="clinica capilar cr7" width="350">
+			<!--<img src="./img/insparya.jpg" alt="clinica capilar cr7" width="350">
+			-->
 			<img src="./img/kaliseParaTodos.png" alt="kalise para todos" width="350">
 			<!--<img src="./img/oreo.jpg" alt="galletas oreo" width="350">-->
 			
