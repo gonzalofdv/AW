46cdea2035c006bd7808d43fc2b71b4a48300fca
juancar99 <jurosado@ucm.es>
2020-03-30 17:47:38 +0200

    Update cabecera.php

--- cabecera.php
+++ cabecera.php
@@ -1,7 +1,17 @@
 <div id="cabecera">
 		<?php
 			if (isset($_SESSION["login"]) && ($_SESSION["login"]==true)) {
-				echo "Hola {$_SESSION['nombre']} <a href='logout.php'>(Cerrar sesión)</a>";
+				echo "Hola {$_SESSION['nombre']} <a href='logout.php'>(Cerrar sesión)</a><br>";
+				if($_SESSION['esAdmin']){
+					echo "Eres administrador";
+				}
+				else if($_SESSION['esFamilia']){
+					echo "Eres SomosFamilia";
+				}
+				else{
+					echo "Eres usuario normal";
+				}
+				
 			}
 			else {
 				echo "<a href='registro.php'>Registrate! </a>" . "<a class=boton_personalizado href=login.php>Accede</a>";
