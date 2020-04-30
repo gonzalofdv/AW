d1fa57213d2b8b981e35089cca179c29f852df39
JorgeBorja <jorborja@ucm.es>
2020-04-30 23:05:25 +0200

    css

--- include/comun/cabecera.php
+++ include/comun/cabecera.php
@@ -1,44 +1,39 @@
 <div id="cabecera">
-		<?php
-			if (isset($_SESSION["login"]) && ($_SESSION["login"]==true)) {
-				echo "Hola {$_SESSION['nombre']} <a href='logout.php'>(Cerrar sesión)</a><br>";
-				if($_SESSION['esAdmin']){
-					echo "Eres administrador";
+	<div id="start">
+			<?php
+				if (isset($_SESSION["login"]) && ($_SESSION["login"]==true)) {
+					echo "Hola {$_SESSION['nombre']} <a href='logout.php'>(Cerrar sesión)</a><br>";
+					if($_SESSION['esAdmin']){
+						echo "Eres administrador";
+					}
+					else if($_SESSION['esFamilia']){
+						echo "Eres SomosFamilia";
+					}
+					else{
+						echo "Eres usuario normal";
+					}
+					
 				}
-				else if($_SESSION['esFamilia']){
-					echo "Eres SomosFamilia";
+				else {
+					echo "<a class=boton_registro href='registro.php'>¡Registrate! </a>" . "<a class=boton_accede href=login.php>Accede</a>";
 				}
-				else{
-					echo "Eres usuario normal";
-				}
-				
-			}
-			else {
-				echo "<a href='registro.php'>Registrate! </a>" . "<a class=boton_personalizado href=login.php>Accede</a>";
-			}
-		?>
-		<img src="./img/ElVARderinDeCornerr.jpg" class="center">
-		<style>
-		.center {
-  				display: block;
-				margin-left: auto;
-				margin-right: auto;
-				width: 30%;
-		}</style>
-		<!--<img src="./img/ElVARderinDeCorner.jpg" position = center>-->
-		
-</div>
-<div id="menu">
-		<button onclick="location.href='index.php'">Inicio</button>
-		<button onclick="location.href='mostrarInicioQuiz.php'">Quiz</button>
-		<button onclick="location.href='somosFamilia.php'">Somos Familia</button>
-</div>
+			?>
+			<img src="./img/ElVARderinDeCornerr.jpg" class="imagenPrincipal">
+			<!--<img src="./img/ElVARderinDeCorner.jpg" position = center>-->
+			
+	</div>
+	<div id="menu">
+			<button onclick="location.href='index.php'">Inicio</button>
+			<button onclick="location.href='mostrarInicioQuiz.php'">Quiz</button>
+			<button onclick="location.href='somosFamilia.php'">Somos Familia</button>
+	</div>
 
-<div id="ligas">
-		<input type="image" onclick="location.href='eleccionLigas.php?codLiga=1'" value="Liga Santander" src="img/ligas/LaLigaSantander.png" />
-		<input type="image" onclick="location.href='eleccionLigas.php?codLiga=2'" value="Liga Smartbank" src="img/ligas/LaLigaSmartbank.png" />
-		<input type="image" onclick="location.href='eleccionLigas.php?codLiga=3'" value="Premier League" src="img/ligas/PremierLeague.png" />
-		<input type="image" onclick="location.href='eleccionLigas.php?codLiga=4'" value="Bundesliga" src="img/ligas/Bundesliga.png" />
-		<input type="image" onclick="location.href='eleccionLigas.php?codLiga=5'" value="Ligue 1" src="img/ligas/Ligue1.png" />
-		<input type="image" onclick="location.href='eleccionLigas.php?codLiga=6'" value="Serie A" src="img/ligas/SerieA.png" />
+	<div id="ligas">
+			<input type="image" onclick="location.href='eleccionLigas.php?codLiga=1'" value="Liga Santander" src="img/ligas/LaLigaSantander.png" />
+			<input type="image" onclick="location.href='eleccionLigas.php?codLiga=2'" value="Liga Smartbank" src="img/ligas/LaLigaSmartbank.png" />
+			<input type="image" onclick="location.href='eleccionLigas.php?codLiga=3'" value="Premier League" src="img/ligas/PremierLeague.png" />
+			<input type="image" onclick="location.href='eleccionLigas.php?codLiga=4'" value="Bundesliga" src="img/ligas/Bundesliga.png" />
+			<input type="image" onclick="location.href='eleccionLigas.php?codLiga=5'" value="Ligue 1" src="img/ligas/Ligue1.png" />
+			<input type="image" onclick="location.href='eleccionLigas.php?codLiga=6'" value="Serie A" src="img/ligas/SerieA.png" />
+	</div>
 </div>
--- include/comun/sidebarDer.php
+++ include/comun/sidebarDer.php
@@ -1,56 +1,56 @@
-	<div id="sidebar-right">
-	
-		<div id="perfil">
-			<?php
-				if (isset($_SESSION["login"]) && ($_SESSION["login"]==true)) {
-			?>
-					<button class="perfil rounded" onclick="location.href='mostrarPerfil.php'"><span class="text-green">Mi perfil</button>
-			<?php
-				}
-			?>
-		</div>
+<div id="sidebar-right">
 
-		<div id="ranking">
-
-			<?php
-
-				require_once './include/sa/UsuarioSA.php';
-
-
-				$usuarioSA = new UsuarioSA();
-				$lista = $usuarioSA->devuelveRanking();
-
-			?>
-
-			<table border="2">
-				<caption><b>TOP 10 USUARIOS</b></caption>
-				<tr>
-					<td><b>Puesto</b></td>	
-					<td><b>Usuario</b></td>
-					<td><b>Puntos</b></td>
-				</tr>
-
-			<?php
-				$i = 1;
-				while($mostrar=mysqli_fetch_object($lista)){
-					echo "<tr>";
-						echo "<td>".$i."º</td>";
-						echo "<td>".$mostrar->NombreUsuario."</td>";
-						echo "<td>".$mostrar->Puntos."</td>";
-					echo "</tr>";
-
-					$i++;
-				}
-
-			?>		
-			</table>
-			<br>
-
-		</div>
-		
-		<div id="anuncios">
-			<img src="./img/insparya.jpg" alt="clinica capilar cr7" width="350">
-			<img src="./img/kaliseParaTodos.png" alt="kalise para todos" width="350">			
-		</div>
-		
+	<div id="perfil">
+		<?php
+			if (isset($_SESSION["login"]) && ($_SESSION["login"]==true)) {
+		?>
+			<button class="perfil rounded" onclick="location.href='mostrarPerfil.php'"><span class="text-green">Mi perfil</button>
+		<?php
+			}
+		?>
 	</div>
+
+	<div id="ranking">
+
+		<?php
+
+			require_once './include/sa/UsuarioSA.php';
+
+
+			$usuarioSA = new UsuarioSA();
+			$lista = $usuarioSA->devuelveRanking();
+
+		?>
+
+		<table class="tablas" border="2">
+			<caption><b>TOP 10 USUARIOS</b></caption>
+			<tr>
+				<td><b>Puesto</b></td>	
+				<td><b>Usuario</b></td>
+				<td><b>Puntos</b></td>
+			</tr>
+
+		<?php
+			$i = 1;
+			while($mostrar=mysqli_fetch_object($lista)){
+				echo "<tr>";
+					echo "<td>".$i."º</td>";
+					echo "<td>".$mostrar->NombreUsuario."</td>";
+					echo "<td>".$mostrar->Puntos."</td>";
+				echo "</tr>";
+
+				$i++;
+			}
+
+		?>		
+		</table>
+		<br>
+
+	</div>
+	
+	<div id="anuncios">
+		<img class="imagenes" src="./img/insparya.jpg" alt="clinica capilar cr7">
+		<img class="imagenes" src="./img/kaliseParaTodos.png" alt="kalise para todos">			
+	</div>
+	
+</div>
