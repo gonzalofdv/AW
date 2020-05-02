5f565b59dcf3cfdf7e9a469cb3363ff46dc83b9e
juancar99 <jurosado@ucm.es>
2020-05-02 18:37:57 +0200

    a

--- mostrarNoticiasEleccion.php
+++ mostrarNoticiasEleccion.php
@@ -6,6 +6,7 @@
 	$noticiaSA = new NoticiaSA();
 	$res = $noticiaSA->devuelveNoticias($codLiga);
 	while($valores = mysqli_fetch_array($res)){
+		echo '<br>';
 		echo '<img src="'.'./img/noticias/'.$valores[5].'" alt="Imagen noticia" width="350">';
 		echo '<h2><a href="mostrarNoticia.php?idN='.$valores[0].'">' . $valores[4] . '</a></h2>';
 		echo '<h3>'.substr($valores[3],0,40) . "...".'</h3>';
