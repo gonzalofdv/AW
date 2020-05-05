2ef34b942ff93961cba4f7d746da081dd1eff014
acernuda <43859036+acernuda@users.noreply.github.com>
2020-05-05 19:44:53 +0200

    Mayo

--- include/comun/pie.php
+++ include/comun/pie.php
@@ -1,3 +1,3 @@
 	<div id="pie">
-		© Marzo 2020 Unidad Editorial Información Deportiva, S.L.U. Todos los derechos reservados.
+		© Mayo 2020 Unidad Editorial Información Deportiva, S.L.U. Todos los derechos reservados.
 	</div>
