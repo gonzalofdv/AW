76e46b0f9896de21689bab43093a4e220f9615cd
juancar99 <jurosado@ucm.es>
2020-05-07 19:25:29 +0200

    Update mostrarNoticiasEleccion.php

--- mostrarNoticiasEleccion.php
+++ mostrarNoticiasEleccion.php
@@ -5,12 +5,15 @@
 
 	$noticiaSA = new NoticiaSA();
 	$res = $noticiaSA->devuelveNoticias($codLiga);
+	echo '<div class="divnoticias">';
 	while($valores = mysqli_fetch_array($res)){
-		echo '<br>';
-		echo '<img src="'.'./img/noticias/'.$valores[5].'" alt="Imagen noticia" width="350">';
+		echo '<div class="noticiaindiv">';
+		echo '<img class="imgNot" src="'.'./img/noticias/'.$valores[5].'" alt="Imagen noticia" width="350">';
 		echo '<h2><a href="mostrarNoticia.php?idN='.$valores[0].'">' . $valores[4] . '</a></h2>';
 		echo '<h3>'.substr($valores[3],0,40) . "...".'</h3>';
 		echo '<br>';
+		echo '</div>';
 	}
+	echo '</div>';
 	
 ?>
