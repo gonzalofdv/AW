88c919e94a9a387ccaaffec22d3a2051e51a6435
juancar99 <jurosado@ucm.es>
2020-05-26 19:02:44 +0200

    quitar subrayado noticias

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ $codLiga = isset($_POST['liga']) ? $_POST['liga'] : 0;
 				while($valores = mysqli_fetch_array($res)){
 					echo '<div class="noticiaindiv">';
 						echo '<img class="imgNot" src="'.'./img/noticias/'.$valores[5].'" alt="Imagen noticia">';
-						echo '<h2><a href="mostrarNoticia.php?idN='.$valores[0].'">' . $valores[4] . '</a></h2>';
+						echo '<h2><a class="enlaceNoticia" href="mostrarNoticia.php?idN='.$valores[0].'">' . $valores[4] . '</a></h2>';
 						echo '<h3>'.substr($valores[3],0,40) . "...".'</h3>';
 
 						echo '<br>';
--- mostrarNoticiasEleccion.php
+++ mostrarNoticiasEleccion.php
@@ -15,7 +15,7 @@
 	while($valores = mysqli_fetch_array($res)){
 		echo '<div class="noticiaindiv">';
 		echo '<img class="imgNot" src="'.'./img/noticias/'.$valores[5].'" alt="Imagen noticia" width="350">';
-		echo '<h2><a href="mostrarNoticia.php?idN='.$valores[0].'">' . $valores[4] . '</a></h2>';
+		echo '<h2><a class="enlaceNoticia "href="mostrarNoticia.php?idN='.$valores[0].'">' . $valores[4] . '</a></h2>';
 		echo '<h3>'.substr($valores[3],0,40) . "...".'</h3>';
 		echo '<br>';
 		echo '</div>';
