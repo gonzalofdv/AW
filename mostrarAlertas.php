74bc04b191e9a428c019d59de0eeea7667d64872
gonzalofdv <gfigue01@ucm.es>
2020-05-27 18:10:23 +0200

    Update mostrarAlertas.php

--- mostrarAlertas.php
+++ mostrarAlertas.php
@@ -1,9 +1,111 @@
 <?php session_start(); 
-
 $codigo = $_GET['codAlerta'];
+switch($codigo){
+	case 1:
+		$titulo = "Acceso denegado!";
+		$mensaje = "Para entrar a esta página es necesario que hayas iniciado sesión, si no tienes cuenta, ¡regístrate!.";
+		$mensaje .= "Te redireccionamos a la página de inicio.";
+		$url ="index.php";			
+		break;
+	case 2:
+		$titulo = "No puedes realizar esta acción";
+		$mensaje = "Si eres un administrador, ¿para qué quieres ser usuario Somos Familia? Tu me has creado a mi, eres todopoderoso.<br>";
+		$mensaje .= "Te redireccionamos...";
+		$url = "somosFamilia.php";
+		break;
+	case 3:
+		$titulo = "No puedes realizar esta acción";
+		$mensaje = "¡Quieto ahí! Ya eres un usuario Somos Familia, sabemos que estás encantado pero no vas a perder los privilegios, no te preocupes <br>";
+		$mensaje .= "Te redireccionamos...";
+		$url = "somosFamilia.php";
+		break;
+	case 4:
+		$titulo = "BIENVENIDO";
+		$mensaje = "Ahora formas parte de nuestra especial familia, comienza ya a disfrutar de todos los privilegios. <br>";
+		$mensaje .= "Te redireccionamos a la página de inicio.";
+		$url = "index.php";
+		break;
+	case 5:
+		$titulo = "Puntos insuficientes";
+		$mensaje = "Sabemos que quieres pertenecer a esta especial familia, pero sigue esforzándote para conseguir puntos.<br>";
+		$mensaje .= "Te redireccionamos a la página de inicio.";
+		$url = "somosFamilia.php";
+		break;
+	case 7:
+		$titulo = "¡Asegúrate de que estás votando correctamente!";
+		$mensaje = "Te redireccionamos a la página de inicio.";
+		$url = "index.php";
+		break;
+	case 8:
+		$titulo = "Usuario registrado correctamente";
+		$mensaje = "A continuación, te llevaremos al apartado de elegir equipo para que puedas seleccionar tu equipo favorito que además completará tu imagen de perfil.";
+		$mensaje .= "<br>Te redireccionamos automáticamente.";
+		$url="cambioEquipo.php";
+		break;
+	case 9:
+		$titulo = "Login con éxito";
+		$mensaje = "Te redireccionamos automáticamente a la página de inicio";
+		$url = "login.php";
+		break;
+	case 12:
+		$titulo = "Nueva noticia agregada correctamente.";
+		$mensaje = "Te redireccionamos a la página de inicio.";
+		$url = "index.php";
+		break;
+	case 13:
+		$titulo = "Algo ha fallado por aquí..." . "<br>";
+		$mensaje = "Te redireccionamos a la página de inicio automáticamente.";
+		$url = "index.php";
+		break;
+	case 15:
+		$titulo = "Gracias, ¡Vuelva pronto!";
+		$mensaje = "Te redireccionamos a la página de inicio.";
+		$url = "index.php";
+		break;
+	case 16:
+		$titulo = "Comentario agregado con éxito";
+		$mensaje = "Te redireccionamos a la página de inicio automáticamente";
+		$url = "index.php"
+		break;
+	case 19:
+		$titulo = "Acción con éxito";
+		$mensaje = "Nueva pregunta insertada a la BBDD correctamente, gracias por colaborar<br>";
+		$mensaje .= "<br>Te redireccionamos automáticamente";
+		$url = "mostrarInicioQuiz.php"
+		break;
+	case 25:
+		$titulo = "Noticia eliminada con éxito";
+		$mensaje = "Te redireccionamos a la página de inicio.";
+		$url = "index.php";
+		break;
+	case 26:
+		$titulo = "Noticia actualizada correctamente";
+		$mensaje = "Te redireccionamos a la página de inicio.";
+		$url = "index.php";
+		break;
+	case 27:
+		$titulo = "Comentario borrado correctamente";
+		$mensaje = "Te redireccionamos a la página de inicio.";
+		$url = "index.php";
+		break;
+	case 28:
+		$titulo = "Has llegado al límite de votos. ¡Vuelve mañana!";
+		$mensaje = "Te redireccionamos a la página de inicio.";
+		$url = "index.php";
+		break;
+	case 29:
+		$titulo = "Sentimos tu marcha. Puedes volver cuando quieras";
+		$mensaje = "Te redireccionamos a la página de inicio.<br>";
+		$mensaje .= '<img src="./img/CuentaEliminada.gif">';
+		$url = "index.php";
+		break;
+	case 30:
+		$titulo = "Usuario actualizado con éxito.";
+		$mensaje = "Te redireccionamos a la página de perfil.";
+		$url = "mostrarPerfil.php";
+		break;
+}	
 ?>
-
-<!DOCTYPE html>
 <html>
 <head>
 	<link rel="stylesheet" type="text/css" href="css/estilo.css" />
@@ -14,6 +116,7 @@ $codigo = $_GET['codAlerta'];
 	<meta http-equiv="Last-Modified" content="0">
 	<meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
 	<meta http-equiv="Pragma" content="no-cache">
+	<meta http-equiv="refresh" content="5; url=<?php echo $url; ?>">
 	<title>Alerta</title>
 </head>
 
@@ -24,145 +127,8 @@ $codigo = $_GET['codAlerta'];
 	?>
 
 	<div class="contenido">
-
-		<?php
-
-			switch($codigo){
-				case 1:
-					echo "<h1>Acceso denegado!</h1>";
-					echo "<p>Para entrar a esta página es necesario que hayas iniciado sesión, si no tienes cuenta, ¡regístrate!.</p>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");				
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 2:
-					echo "<p>Si eres un administrador, ¿para qué quieres ser usuario Somos Familia? Tu me has creado a mi, eres todopoderoso ;)</p>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 3:
-					echo "<p>¡Quieto ahí! Ya eres un usuario Somos Familia, sabemos que estás encantado pero no vas a perder los privilegios, no te preocupes</p>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 4:
-					echo "<h1>BIENVENIDO</h1>";
-					echo "<p>Ahora formas parte de nuestra especial familia, comienza ya a disfrutar de todos los privilegios.</p>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 5:
-					echo "<h1>Puntos insuficientes</h1>";
-					echo "<p>Sabemos que quieres pertenecer a esta especial familia, pero sigue esforzándote para conseguir puntos.</p>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 6:
-					echo "<h1>Permisos insuficientes</h1>";
-					echo "<p>Para poder introducir una nueva pregunta para el Quiz necesitas ser un Administrador o tener los privilegios de usuario Somos Familia que podrás conseguir colaborando en nuestra página. Infórmate más en la sección Somos Familia.</p>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 7:
-					echo "<h1>¡Asegúrate de que estás votando correctamente!</h1>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 8:
-					echo "Usuario registrado correctamente. A continuación, te llevaremos al apartado de elegir equipo para que puedas seleccionar tu equipo favorito que además completará tu imagen de perfil.";
-					echo "<p>Te redireccionamos con el siguiente botón.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="cambioEquipo.php">Elegir equipo</button>';
-					break;
-				case 9:
-					echo "El usuario introducido ya existe";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="registro.php">Volver al registro</button>';
-					break;
-				case 12:
-					echo "Nueva noticia agregada correctamente.<br>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 13:
-					echo "Algo ha fallado por aquí..." . "<br>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 15:
-					echo "Gracias, ¡Vuelva pronto!" . "<br>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 16:
-					echo "El comentario se ha agregado con éxito" . "<br>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 19:
-					echo "Nueva pregunta insertada a la BBDD correctamente, gracias por colaborar<br>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 20:
-					echo "Se ha producido un fallo al introducir la pregunta";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 25:
-					echo "<h1>Noticia eliminada con éxito</h1>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 26:
-					echo "<h1>Noticia actualizada correctamente</h1>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 27:
-					echo "<h1>Comentario borrado correctamente</h1>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 28:
-					echo "<h1>Has llegado al límite de votos. ¡Vuelve mañana!</h1>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 29:
-					echo "<h1>Sentimos tu marcha. Puedes volver cuando quieras</h1>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					echo '<img src="./img/CuentaEliminada.gif"><br>';
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-				case 30:
-					echo "<h1>Usuario actualizado con éxito.</h1>";
-					echo "<p>Te redireccionamos a la página de inicio.</p>";
-					//header("refresh:5; url=index.php");
-					echo '<button class="botGen" onclick=location.href="index.php">Volver al inicio</button>';
-					break;
-			}	
-
-		?>
-
-
+		<h2><?php echo $titulo;	?></h2>
+		<h3><?php echo $mensaje; ?></h3>
 	</div>
 
 	<?php
