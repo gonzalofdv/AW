56716a7eb1e69f9893321e9ceae661569a37dda7
gonzalofdv <gfigue01@ucm.es>
2020-05-28 11:32:03 +0200

    bases

---
+++ mysql/01crearTablas.sql
@@ -0,0 +1,410 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 28-05-2020 a las 11:16:47
+-- Versión del servidor: 10.4.6-MariaDB
+-- Versión de PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `varderindecorner`
+--
+CREATE DATABASE IF NOT EXISTS `varderindecorner` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `varderindecorner`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
+--
+
+CREATE TABLE `comentarios` (
+  `IdComentario` int(6) NOT NULL,
+  `CodNoticia` int(6) NOT NULL,
+  `CodUsuario` int(6) NOT NULL,
+  `Comentario` text NOT NULL,
+  `MeGustas` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `equipos`
+--
+
+CREATE TABLE `equipos` (
+  `IdEquipo` int(6) NOT NULL,
+  `nombreEquipo` varchar(30) NOT NULL,
+  `CodLiga` int(6) NOT NULL,
+  `Puntos` int(3) NOT NULL,
+  `GolesAFavor` int(3) NOT NULL,
+  `GolesEnContra` int(3) NOT NULL,
+  `Escudo` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `jugadores`
+--
+
+CREATE TABLE `jugadores` (
+  `IdJugador` int(6) NOT NULL,
+  `CodEquipo` int(6) NOT NULL,
+  `Nombre` varchar(20) NOT NULL,
+  `Apellido` varchar(20) NOT NULL,
+  `Apodo` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ligas`
+--
+
+CREATE TABLE `ligas` (
+  `IdLiga` int(6) NOT NULL,
+  `Nombre` varchar(40) NOT NULL,
+  `Pais` varchar(20) NOT NULL,
+  `NEquipos` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `noticias`
+--
+
+CREATE TABLE `noticias` (
+  `IdNoticia` int(11) NOT NULL,
+  `CodUsuario` int(6) NOT NULL,
+  `CodLiga` int(6) NOT NULL,
+  `Texto` text CHARACTER SET utf8 NOT NULL,
+  `Titular` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `Foto` varchar(1000) CHARACTER SET utf8 NOT NULL,
+  `NotaNoticia` float NOT NULL,
+  `NumVotos` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `opcionesvotacion`
+--
+
+CREATE TABLE `opcionesvotacion` (
+  `IdOpcion` int(6) NOT NULL,
+  `CodVotacion` int(6) NOT NULL,
+  `CodJugador` int(6) NOT NULL,
+  `NumVotos` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `preguntas`
+--
+
+CREATE TABLE `preguntas` (
+  `IdPregunta` int(3) NOT NULL,
+  `Pregunta` varchar(300) NOT NULL,
+  `CodLiga` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuestas`
+--
+
+CREATE TABLE `respuestas` (
+  `IdRespuesta` int(3) NOT NULL,
+  `CodPregunta` int(3) NOT NULL,
+  `Respuesta` varchar(100) NOT NULL,
+  `Correcta` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `respuestascomentario`
+--
+
+CREATE TABLE `respuestascomentario` (
+  `IdRespuestaComentario` int(4) NOT NULL,
+  `CodComentario` int(4) NOT NULL,
+  `CodUsuario` int(4) NOT NULL,
+  `Texto` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `IdUsuario` int(10) NOT NULL,
+  `Nombre` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `Apellido1` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `Apellido2` varchar(30) CHARACTER SET utf8 NOT NULL,
+  `Sexo` varchar(10) CHARACTER SET utf8 NOT NULL,
+  `EquipoFavorito` int(4) NOT NULL,
+  `NombreUsuario` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `Contrasena` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `Email` varchar(100) CHARACTER SET utf8 NOT NULL,
+  `Administrador` tinyint(1) NOT NULL,
+  `SomosFamilia` tinyint(1) NOT NULL,
+  `Puntos` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `votaciones`
+--
+
+CREATE TABLE `votaciones` (
+  `IdVotacion` int(6) NOT NULL,
+  `CodLiga` int(6) NOT NULL,
+  `Titulo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD PRIMARY KEY (`IdComentario`),
+  ADD KEY `index` (`CodNoticia`,`CodUsuario`),
+  ADD KEY `CodUsuario` (`CodUsuario`);
+
+--
+-- Indices de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  ADD PRIMARY KEY (`IdEquipo`),
+  ADD KEY `IdLiga` (`CodLiga`);
+
+--
+-- Indices de la tabla `jugadores`
+--
+ALTER TABLE `jugadores`
+  ADD PRIMARY KEY (`IdJugador`),
+  ADD KEY `IdEquipo` (`CodEquipo`);
+
+--
+-- Indices de la tabla `ligas`
+--
+ALTER TABLE `ligas`
+  ADD PRIMARY KEY (`IdLiga`);
+
+--
+-- Indices de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  ADD PRIMARY KEY (`IdNoticia`),
+  ADD KEY `CodUsuario` (`CodUsuario`),
+  ADD KEY `CodLiga` (`CodLiga`);
+
+--
+-- Indices de la tabla `opcionesvotacion`
+--
+ALTER TABLE `opcionesvotacion`
+  ADD PRIMARY KEY (`IdOpcion`),
+  ADD KEY `CodVotacion` (`CodVotacion`),
+  ADD KEY `CodJugador` (`CodJugador`);
+
+--
+-- Indices de la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  ADD PRIMARY KEY (`IdPregunta`),
+  ADD KEY `IdLiga` (`CodLiga`);
+
+--
+-- Indices de la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  ADD PRIMARY KEY (`IdRespuesta`),
+  ADD KEY `IdPregunta` (`CodPregunta`);
+
+--
+-- Indices de la tabla `respuestascomentario`
+--
+ALTER TABLE `respuestascomentario`
+  ADD PRIMARY KEY (`IdRespuestaComentario`),
+  ADD KEY `CodComentario` (`CodComentario`),
+  ADD KEY `CodUsuario` (`CodUsuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`IdUsuario`),
+  ADD KEY `EquipoFavorito` (`EquipoFavorito`);
+
+--
+-- Indices de la tabla `votaciones`
+--
+ALTER TABLE `votaciones`
+  ADD PRIMARY KEY (`IdVotacion`),
+  ADD KEY `CodLiga` (`CodLiga`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  MODIFY `IdComentario` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  MODIFY `IdEquipo` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `jugadores`
+--
+ALTER TABLE `jugadores`
+  MODIFY `IdJugador` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ligas`
+--
+ALTER TABLE `ligas`
+  MODIFY `IdLiga` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  MODIFY `IdNoticia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `opcionesvotacion`
+--
+ALTER TABLE `opcionesvotacion`
+  MODIFY `IdOpcion` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  MODIFY `IdPregunta` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  MODIFY `IdRespuesta` int(3) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `respuestascomentario`
+--
+ALTER TABLE `respuestascomentario`
+  MODIFY `IdRespuestaComentario` int(4) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `IdUsuario` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `votaciones`
+--
+ALTER TABLE `votaciones`
+  MODIFY `IdVotacion` int(6) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`CodUsuario`) REFERENCES `usuarios` (`IdUsuario`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`CodNoticia`) REFERENCES `noticias` (`IdNoticia`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `equipos`
+--
+ALTER TABLE `equipos`
+  ADD CONSTRAINT `equipos_ibfk_1` FOREIGN KEY (`CodLiga`) REFERENCES `ligas` (`IdLiga`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `jugadores`
+--
+ALTER TABLE `jugadores`
+  ADD CONSTRAINT `jugadores_ibfk_1` FOREIGN KEY (`CodEquipo`) REFERENCES `equipos` (`IdEquipo`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `noticias`
+--
+ALTER TABLE `noticias`
+  ADD CONSTRAINT `noticias_ibfk_1` FOREIGN KEY (`CodUsuario`) REFERENCES `usuarios` (`IdUsuario`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `noticias_ibfk_2` FOREIGN KEY (`CodLiga`) REFERENCES `ligas` (`IdLiga`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `opcionesvotacion`
+--
+ALTER TABLE `opcionesvotacion`
+  ADD CONSTRAINT `opcionesvotacion_ibfk_1` FOREIGN KEY (`CodVotacion`) REFERENCES `votaciones` (`IdVotacion`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `opcionesvotacion_ibfk_2` FOREIGN KEY (`CodJugador`) REFERENCES `jugadores` (`IdJugador`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `preguntas`
+--
+ALTER TABLE `preguntas`
+  ADD CONSTRAINT `preguntas_ibfk_1` FOREIGN KEY (`CodLiga`) REFERENCES `ligas` (`IdLiga`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `respuestas`
+--
+ALTER TABLE `respuestas`
+  ADD CONSTRAINT `respuestas_ibfk_1` FOREIGN KEY (`CodPregunta`) REFERENCES `preguntas` (`IdPregunta`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `respuestascomentario`
+--
+ALTER TABLE `respuestascomentario`
+  ADD CONSTRAINT `respuestascomentario_ibfk_1` FOREIGN KEY (`CodUsuario`) REFERENCES `usuarios` (`IdUsuario`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `respuestascomentario_ibfk_2` FOREIGN KEY (`CodComentario`) REFERENCES `comentarios` (`IdComentario`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`EquipoFavorito`) REFERENCES `equipos` (`IdEquipo`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `votaciones`
+--
+ALTER TABLE `votaciones`
+  ADD CONSTRAINT `votaciones_ibfk_1` FOREIGN KEY (`CodLiga`) REFERENCES `ligas` (`IdLiga`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
